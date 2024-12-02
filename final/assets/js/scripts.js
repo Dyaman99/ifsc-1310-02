54bcaed045217c86dc16c823b70afec8d45e0c13
Dyaman99 <dyaman@ualr.edu>
2024-12-02 02:58:18 +0000

    worked on my final project

--- final/assets/js/scripts.js
+++ final/assets/js/scripts.js
@@ -1,15 +1,23 @@
 $(document).ready(function() {
 
   $('.fade').slick({
+    slidesToShow: 2,
+    slidesToScroll: 2,
     dots: true,
-    infinite: true,
-    speed: 500,
-    fade: true,
-    slide: 'div',
-    cssEase: 'linear',
     autoplay: true,
-    autoplaySpeed: 2000
+    autoplaySpeed: 2000,
   });
 
+  // $('.fade').slick({
+  //   dots: true,
+  //   infinite: true,
+  //   speed: 500,
+  //   fade: true,
+  //   slide: 'div',
+  //   cssEase: 'linear',
+  //   autoplay: true,
+  //   autoplaySpeed: 3000
+  // });
 
 });
+
